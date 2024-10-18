a14e6600e6a440c8e5d7fce16ea3c8b0e792fc1a
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-10-18 15:41:06 -0600

    Creacion de pantalla del juego game.py
    se crea el entorno donde se encontrara el juego

--- game.py
+++ game.py
@@ -2,3 +2,13 @@
 import py game
 
 #pip install pygame
+#Comenzamos con el desarrollo
+ANCHO = 1000
+ALTO = 800
+VENTANA = pygame.display.set_mode
+([ANCHO, ALTO])
+
+jugando = True
+
+while jugando:
+  pygame.display.update()

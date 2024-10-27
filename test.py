6eec0c35461db56783a6b0b9f46c0ad523c5c52b
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-10-26 18:44:15 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 en esto es un juego elaborado con la libreria de pygame en python 
 dicho juego se desarrollara en un tiempo de 1 mes
 este juego sera el primero que voy a desarrollar de muchos
+pero para ello se debe entender los conceptos basicos

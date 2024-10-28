7958e9b77acb58ad72b5e0b82b3ad5cec9c0507a
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-10-28 11:33:31 -0600

    Update personaje.py

--- personaje.py
+++ personaje.py
@@ -5,6 +5,7 @@ class Cubo:
     self.x = y
     self.y = y
     self.ancho = 50
+    self.alto = 50
     self.velocidad = 1
     self.color = "red"
     self.rect = pygame.React(self.x, self.y, self.ancho, self.alto)

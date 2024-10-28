7f93560e272b74545eb821aae07f155e9a3a2985
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-10-28 11:33:02 -0600

    Update enemigo.py

--- enemigo.py
+++ enemigo.py
@@ -1,12 +1,13 @@
 import pygame
 
-class Cubo:
+class Enemigo:
   def __init__(self, x, y):
     self.x = y
     self.y = y
     self.ancho = 50
-    self.velocidad = 1
-    self.color = "red"
+    self.alto = 50
+    self.velocidad = .33
+    self.color = "puerple"
     self.rect = pygame.React(self.x, self.y, self.ancho, self.alto)
 
   def dibujar (self, ventana):
